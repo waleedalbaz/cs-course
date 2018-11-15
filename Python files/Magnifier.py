40119fd41cea63debcd6dfa6cf2bad0a3addffab
Waleed <walllaid@gmail.com>
2018-11-15 04:33:47 +0300

    a lot of work into it

--- Python files/Magnifier.py
+++ Python files/Magnifier.py
@@ -22,38 +22,94 @@ print 'MAG_POS', MAG_POS
 print "W =", CANV_SIZE[0]
 print "H =", CANV_SIZE[1]
 
-# class
-class Magnifier()
-def __init__(self, image, size, center):
-	self.image = image
-	self.size = [size[0], size[1]]
-	self.center = [center[0], center[1]]
-	self.degree = self.size
+# classes
+class ImageInfo():
+    """
+    container for all image info needed
+    """
+    def __init__(self, center, size):
+        self.center = [center[0], center[1]]
+        self.size = [size[0], size[1]]
 
-def set_degree(self, degree):
-	"""
-	change the degree of zoomming
-	"""
-	self.degree = degree
+    def get_center(self):
+        """
+        return image center value
+        """
+        return self.center
 
+    def get_size(self):
+        """
+        return image size value
+        """
+        return self.size
+
+class Magnifier():
+    """
+    create a magnifier glass
+    """
+    def __init__(self, image, pos, size):
+        self.image = image
+        self.pos = pos
+        self.size = size
+        self.degree = 3.0
+        self.center = [self.degree*self.pos[0], self.degree*self.pos[1]]
+
+    def set_pos(self, pos):
+        self.pos = pos
+
+    def set_degree(self, degree):
+        """
+        change the degree of zoomming
+        """
+        self.degree = degree #TODO
+
+    def get_position(self):
+        """
+        get the position
+        """
+        return self.pos
+
+    def get_degree(self):
+        """
+        get the degree of zoomming
+        """
+        return self.degree
+
+    def get_center(self):
+        return self.center
+    
+    def get_size(self):
+        return self.size
+
+# objects
+# image info
+image_info = ImageInfo(CENT_IMG, [W_IMG, H_IMG])
+
+# magnifier
+Magnifier_glass = Magnifier(image, MAG_POS, MAG_SIZE)
 
 # event handlers
 def click(pos):
-    global MAG_POS
-    MAG_POS = list(pos)
+    # global MAG_POS
+    # MAG_POS = list(pos)
+    Magnifier_glass.set_pos(list(pos))
 
 def draw(canvas):
-    canvas.draw_image(image,
-                      [W_IMG//2, H_IMG//2], [W_IMG, H_IMG],
-                      [CANV_SIZE[0]//2, CANV_SIZE[1]//2],CANV_SIZE)
+#    canvas.draw_image(image,
+                  #    [W_IMG//2, H_IMG//2], [W_IMG, H_IMG],
+                 #     [CANV_SIZE[0]//2, CANV_SIZE[1]//2],CANV_SIZE)
+    canvas.draw_image(image, image_info.get_center(), image_info.get_size(),
+                        [CANV_SIZE[0]//2, CANV_SIZE[1]//2], CANV_SIZE)
     # draw mag
-    map_center = [SCALE * MAG_POS[0], SCALE * MAG_POS[1]]
-    map_rectangle = [MAG_SIZE, MAG_SIZE]
-    mag_center = MAG_POS
-    mag_rectangle = [MAG_SIZE, MAG_SIZE]
-    canvas.draw_image(image,
-                      map_center, map_rectangle,
-                      mag_center, mag_rectangle)
+    map_center = Magnifier_glass.get_center()
+    map_rectangle = Magnifier_glass.get_size()
+    mag_center = Magnifier_glass.get_position()
+    mag_rectangle = Magnifier_glass.get_size()
+    print "map_center: "+str(map_center)
+    print "map_rectangle: "+str(map_rectangle)
+    print "mag_rectangle: "+str(mag_rectangle)
+    canvas.draw_image(image, map_center, [map_rectangle, map_rectangle],
+                      mag_center, [mag_rectangle, mag_rectangle])
 
 # create frame and its handler rigsters
 f = simplegui.create_frame("Magnifier", CANV_SIZE[0], CANV_SIZE[1])
